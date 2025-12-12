c61e5e7b8aae4960e7d0e6dc54a9f7489e8fc9e6
Sabrina LAALA <laalasabrina7@gmail.com>
2025-12-12 14:33:53 +0100

    Add files via upload

---
+++ UEL313-Groupe5-S3-master (1)/UEL313-Groupe5-S3-master/app/routes.php
@@ -0,0 +1,48 @@
+<?php
+
+// Home page
+$app->get('/', "Watson\Controller\HomeController::indexAction")->bind('home');
+
+// Detailed info about a link
+$app->match('/link/{id}', "Watson\Controller\HomeController::linkAction")->bind('link');
+
+// List links with same tag
+$app->match('/tag/{id}', "Watson\Controller\HomeController::tagAction")->bind('tag');
+
+// Login form
+$app->get('/login', "Watson\Controller\HomeController::loginAction")->bind('login');
+
+
+
+
+
+
+// Admin zone
+$app->match('/admin', "Watson\Controller\AdminController::indexAction")->bind('admin'); //j'ai remplacé get par match prcq match accepte les paramètres GET
+
+
+
+
+
+
+
+// Add a new link
+$app->match('/admin/link/add', "Watson\Controller\AdminController::addLinkAction")->bind('admin_link_add');
+
+// Edit an existing link
+$app->match('/admin/link/{id}/edit', "Watson\Controller\AdminController::editLinkAction")->bind('admin_link_edit');
+
+// Remove a link
+$app->get('/admin/link/{id}/delete', "Watson\Controller\AdminController::deleteLinkAction")->bind('admin_link_delete');
+
+// Add a user
+$app->match('/admin/user/add', "Watson\Controller\AdminController::addUserAction")->bind('admin_user_add');
+
+// Edit an existing user
+$app->match('/admin/user/{id}/edit', "Watson\Controller\AdminController::editUserAction")->bind('admin_user_edit');
+
+// Remove a user
+$app->get('/admin/user/{id}/delete', "Watson\Controller\AdminController::deleteUserAction")->bind('admin_user_delete');
+
+// RSS road
+$app->get('/rss', "Watson\Controller\HomeController::indexActionRss")->bind('rss');
---
+++ UEL313-Groupe5-S3-master (1)/UEL313-Groupe5-S3-master/src/Controller/HomeController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Watson\Controller;
+
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeController {
+
+    /**
+     * Home page controller.
+     *
+     * @param Application $app Silex application
+     */
+    public function indexAction(Application $app) {
+        $links = $app['dao.link']->findAll();
+        return $app['twig']->render('index.html.twig', array('links' => $links));
+    }
+
+    /**
+     * Link details controller.
+     *
+     * @param integer $id Link id
+     * @param Request $request Incoming request
+     * @param Application $app Silex application
+     */
+    public function linkAction($id, Request $request, Application $app) {
+        $link = $app['dao.link']->find($id);
+        if ($app['security.authorization_checker']->isGranted('IS_AUTHENTICATED_FULLY')) {
+            // A user is fully authenticated : he can add comments
+            // Check if he's author for manage link
+
+        }
+        return $app['twig']->render('link.html.twig', array(
+            'link' => $link
+        ));
+    }
+
+    /**
+     * Links by tag controller.
+     *
+     * @param integer $id Tag id
+     * @param Request $request Incoming request
+     * @param Application $app Silex application
+     */
+    public function tagAction($id, Request $request, Application $app) {
+        $links = $app['dao.link']->findAllByTag($id);
+        $tag   = $app['dao.tag']->findTagName($id);
+
+        return $app['twig']->render('result_tag.html.twig', array(
+            'links' => $links,
+            'tag'   => $tag
+        ));
+    }
+
+    /**
+     * User login controller.
+     *
+     * @param Request $request Incoming request
+     * @param Application $app Silex application
+     */
+    public function loginAction(Request $request, Application $app) {
+        return $app['twig']->render('login.html.twig', array(
+            'error'         => $app['security.last_error']($request),
+            'last_username' => $app['session']->get('_security.last_username'),
+            )
+        );
+    }
+
+    /**
+     * rss page controller.
+     *
+     * @param Application $app Silex application
+     */
+    public function indexActionRss(Application $app) {
+        $links = $app['dao.link']->findAll();
+
+        // On définit d'avance le nombre max d'articles dans le RSS
+        $objectif = 15;
+
+        // On compte le nombre d'éléments présents dans notre tableau $links
+        $count = count($links); // = 16
+
+        // On va venir compter la différence entre le nombre d'éléments et l'objectif final
+        $difference = $count - $objectif;
+
+        // tant que nous avons une différence entre le nombre total d'éléments et l'objectif, on en retire pour
+        // arriver à 15 valeurs.
+        while ($difference > 0) {
+            $effaceur = array_pop($links);
+            $difference--;
+        }
+
+        // Penser à transformer avec array_slice ?
+
+        return new Response(
+            $app['twig']->render('rss.xml.twig', array(
+                'links' => $links,
+            )),
+            200,
+            array('Content-Type' => 'application/rss+xml; charset=UTF-8')
+            // On indique que c'est du XML et pas du HTML sous peine que ça cause des erreurs
+        );
+    }
+}
